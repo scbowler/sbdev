88c2f59df1bed82ecd21a843b4920b5dcb8da3a0
Scott Ninja Bowler <scott.bowler@learningfuze.com>
2016-10-01 12:18:35 -0700

    Updated shell commands

--- github_update.php
+++ github_update.php
@@ -1,5 +1,5 @@
 <?php
 
-echo shell_exec( 'cd /var/www/dev.sbdev && git pull origin dev' );
+echo shell_exec( 'cd /var/www/dev.sbdev && sudo git fetch --all && sudo git checkout dev && sudo git pull origin dev' );
 
 echo '<h1>code ran</h1>';
