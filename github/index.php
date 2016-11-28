77132a5374f1f3916fc1c23a6289cf4e42c24eb5
Scott Ninja Bowler <scott.bowler@learningfuze.com>
2016-11-28 12:30:22 -0800

    Updated ouput in github file

--- github/index.php
+++ github/index.php
@@ -185,6 +185,7 @@ if ($headers['X-GitHub-Event'] == 'pull_request'
             //pull latest changes
             exec('git pull', $output, $r);
 
+            $log->info('After pull: ' . $output . ' And $r: ' . $r);
             //change premissions on the .git directory
             exec('chmod -R 0770 .git');
         } else {
